b6bd04b5e253da4985f6091378ef283e4a49b377
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2025-10-19 23:19:44 +0530

    Added project details and swiggy logo

--- site/src/jsMain/kotlin/com/bibek/webproject/components/ExperienceCard.kt
+++ site/src/jsMain/kotlin/com/bibek/webproject/components/ExperienceCard.kt
@@ -6,6 +6,8 @@ import com.bibek.webproject.models.Theme
 import com.bibek.webproject.utils.Constants.FONT_FAMILY
 import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.css.functions.RadialGradient
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
@@ -15,9 +17,16 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.fa.FaCircle
+import com.varabyte.kobweb.silk.components.icons.fa.FaShapes
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.shapes.Circle
+import com.varabyte.kobweb.silk.theme.shapes.CirclePath
+import com.varabyte.kobweb.silk.theme.shapes.Shape
+import com.varabyte.kobweb.silk.theme.shapes.clip
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -94,52 +103,64 @@ fun ExperienceDetails(
         if (breakpoint >= Breakpoint.MD) {
             ExperienceNumber(active = active, experience = experience)
         }
-        Column(
+        Row(
             modifier = Modifier
                 .fillMaxSize()
-                .margin(left = if(breakpoint <= Breakpoint.SM) 0.px else animatedMargin)
+                .margin(left = if (breakpoint <= Breakpoint.SM) 0.px else animatedMargin)
                 .transition(
                     CSSTransition(
                         property = "margin",
                         duration = 500.ms,
                         delay = experience.ordinal * 100.ms
                     )
-                )
-            ,
-            verticalArrangement = Arrangement.Center
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
         ) {
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontFamily(FONT_FAMILY)
-                    .fontSize(20.px)
-                    .fontWeight(FontWeight.Bold)
-                    .color(Theme.Secondary.rgb)
-                    .toAttrs()
-            ) {
-                Text(experience.jobPosition)
-            }
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontFamily(FONT_FAMILY)
-                    .fontSize(14.px)
-                    .fontWeight(FontWeight.Normal)
-                    .color(Theme.Secondary.rgb)
-                    .toAttrs()
-            ) {
-                Text("${experience.from} - ${experience.to}")
-            }
-            P(
-                attrs = Modifier
-                    .margin(topBottom = 0.px)
-                    .fontFamily(FONT_FAMILY)
-                    .fontSize(14.px)
-                    .fontWeight(FontWeight.Normal)
-                    .color(Theme.Primary.rgb)
-                    .toAttrs()
-            ) {
-                Text(experience.company)
+            Image(
+                modifier = Modifier
+                    .size(60.px)
+                    .objectFit(ObjectFit.Cover)
+                    .margin(right = 15.px)
+                    .borderRadius(30.percent),
+                src = experience.companyLogo,
+                desc = "Portfolio Image"
+            )
+            Column {
+                P(
+                    attrs = Modifier
+                        .margin(topBottom = 0.px)
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(20.px)
+                        .fontWeight(FontWeight.Bold)
+                        .color(Theme.Secondary.rgb)
+                        .toAttrs()
+                ) {
+                    Text(experience.jobPosition)
+                }
+                P(
+                    attrs = Modifier
+                        .margin(topBottom = 0.px)
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(14.px)
+                        .fontWeight(FontWeight.Normal)
+                        .color(Theme.Secondary.rgb)
+                        .toAttrs()
+                ) {
+                    Text("${experience.from} - ${experience.to}")
+                }
+                P(
+                    attrs = Modifier
+                        .margin(topBottom = 0.px)
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(14.px)
+                        .fontWeight(FontWeight.Normal)
+                        .color(Theme.Primary.rgb)
+                        .toAttrs()
+                ) {
+                    Text(experience.company)
+                }
+
             }
         }
     }
--- site/src/jsMain/kotlin/com/bibek/webproject/models/Projects.kt
+++ site/src/jsMain/kotlin/com/bibek/webproject/models/Projects.kt
@@ -4,32 +4,37 @@ import com.bibek.webproject.utils.Res
 enum class Projects(
     val image: String,
     val title: String,
-    val description: String
+    val description: String,
+    val link: String
 ) {
     One(
-        image = Res.Image.heart,
-        title = "Where is my Heart",
-        description = "Android & Ktor"
-
+        image = Res.Image.byteStream,
+        title = "Byte Stream",
+        description = "A file downloading library for Android",
+        link = "https://github.com/bibekanandan892/Byte-Stream"
     ),
     Two(
-        image = Res.Image.boruto,
-        title = "Boruto: Naruto Next Generations App",
-        description = "Compose UI Android"
+        image = Res.Image.recipe_database,
+        title = "Recipe Database",
+        description = "A Meal Planning App with notifications and reminders.",
+        link = "https://github.com/bibekanandan892/Recipe-Database"
     ),
     Three(
-        image = Res.Image.portfolio3,
-        title = "NFT Application",
-        description = "Frontend/Backend"
+        image = Res.Image.heart,
+        title = "Where is my Heart",
+        description = "Android & Ktor",
+        link = "https://github.com/bibekanandan892/Where_is_my_heart"
     ),
     Four(
-        image = Res.Image.portfolio4,
-        title = "Game Statistics Application",
-        description = "Web/Mobile App"
+        image = Res.Image.portfolio3,
+        title = "NFT Application",
+        description = "Frontend/Backend",
+        link = ""
     ),
     Five(
         image = Res.Image.portfolio5,
         title = "Platform for Online Courses",
-        description = "Web/Mobile App"
+        description = "Web/Mobile App",
+        link = ""
     )
 }
